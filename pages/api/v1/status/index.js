8f55d90b693682ae1124b5c6c67b1a8276ea35ba
devmatsu <devmatsu@hotmail.com>
2024-03-06 00:29:46 -0300

    add 3 new fields into status route (postgres version, max connections and used connections)

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,7 +2,21 @@ import database from "infra/database.js";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
-  response.status(200).json({ updated_at: updatedAt });
+
+  const postgresVersion = await database.query("SHOW server_version;");
+  const postgresMaxConnections = await database.query("SHOW max_connections;");
+  const postgresUsedConnections = await database.query(
+    "SELECT * FROM pg_stat_activity;",
+  );
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    postgres_version: postgresVersion.rows[0].server_version,
+    postgres_max_connections: parseInt(
+      postgresMaxConnections.rows[0].max_connections,
+    ),
+    postgres_used_connections: postgresUsedConnections.rowCount,
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,8 +3,13 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
+  console.log(responseBody);
   expect(responseBody.updated_at).toBeDefined();
 
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.postgres_version).toBeDefined();
+  expect(responseBody.postgres_max_connections).toBeDefined();
+  expect(responseBody.postgres_used_connections).toBeDefined();
 });
