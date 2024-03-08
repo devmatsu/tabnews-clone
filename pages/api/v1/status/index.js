2cf9a6b9315769e56bda088e44959314cbf55aa2
devmatsu <devmatsu@hotmail.com>
2024-03-08 00:52:19 -0300

    refactoring code

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,20 +3,25 @@ import database from "infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  const postgresVersion = await database.query("SHOW server_version;");
-  const postgresMaxConnections = await database.query("SHOW max_connections;");
-  const postgresUsedConnections = await database.query(
+  const dbVersion = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersion.rows[0].server_version;
+
+  const dbMaxConnections = await database.query("SHOW max_connections;");
+  const dbMaxConnectionsValue = parseInt(
+    dbMaxConnections.rows[0].max_connections,
+  );
+
+  const dbUsedConnections = await database.query(
     "SELECT * FROM pg_stat_activity;",
   );
+  const dbUsedConnectionsValue = dbUsedConnections.rowCount;
 
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
-        version: postgresVersion.rows[0].server_version,
-        max_connections: parseInt(
-          postgresMaxConnections.rows[0].max_connections,
-        ),
+        version: dbVersionValue,
+        max_connections: dbMaxConnectionsValue,
       },
     },
   });
